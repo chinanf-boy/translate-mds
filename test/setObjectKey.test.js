5dad155de52d8301a0aa90bedfb1af4f34830fe2
chinanf-boy <865501259@qq.com>
2017-11-20 17:59:40 +0800

    add test

--- test/setObjectKey.test.js
+++ test/setObjectKey.test.js
@@ -4,7 +4,12 @@ const [tree, truetree ] = require('./setObjectKey.Object.js')
 const setObjectKey = require('../src/setObjectKey.js')
 
 test(' test ',async t =>{
-    let newTree = await setObjectKey(tree)
+    let newTree = await setObjectKey(tree, 'baidu')
     t.deepEqual(newTree, truetree)
 })
 
+
+test(' test youdao',async t =>{
+    let newTree = await setObjectKey(tree, 'youdao')
+    t.deepEqual(newTree, truetree)
+})
