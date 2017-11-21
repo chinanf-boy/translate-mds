7db620c492885b046df6740b6c3a7e9fd5b38fe5
chinanf-boy <865501259@qq.com>
2017-11-21 10:26:43 +0800

    fix setObjectkey.test

--- test/setObjectKey.test.js
+++ test/setObjectKey.test.js
@@ -3,13 +3,16 @@ const fs = require('fs')
 const [tree, truetree ] = require('./setObjectKey.Object.js')
 const setObjectKey = require('../src/setObjectKey.js')
 
+var newObject = (oldObject) =>JSON.parse(JSON.stringify(oldObject));
+
 test(' test ',async t =>{
-    let newTree = await setObjectKey(Array.from(tree), 'baidu')
-    t.deepEqual(newTree, Array.from(truetree))
+    let newTree = await setObjectKey(newObject(tree), 'baidu')
+    console.log(JSON.stringify(newTree))
+    t.deepEqual(newTree, truetree)
 })
 
 
 test(' test youdao',async t =>{
-    let newTree = await setObjectKey(Array.from(tree), 'youdao')
-    t.deepEqual(newTree, Array.from(truetree))
+    let newTree = await setObjectKey(newObject(tree), 'youdao')
+    t.deepEqual(newTree, truetree)
 })
