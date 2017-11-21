ee200aee9835a4ef6a5f6c7894a82bcacdbb42a6
chinanf-boy <865501259@qq.com>
2017-11-21 19:14:19 +0800

    add config json

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+( async function(){
 'use script'
 const fs = require('fs')
 const tjs = require('translation.js')
@@ -9,36 +10,53 @@ const chalk = require('chalk');
 const cutMdhead = require('./src/cutMdhead.js')
 const remark = require('remark')
 const {setObjectKey} = require('./src/setObjectKey.js')
+const { logger } = require('./config/loggerConfig.js') // winston config
+let defaultJson = './config/defaultConfig.json' // default config---
+let jsonText = require(defaultJson) //---
+const writeJson  = require('./util/writeJson.js')
 // const debuglog 
 // next ready auto select api source
 // cli cmd 
 const cli = meow(`
 Usage
-  $ translate-md-content [folder name] [API]{google,baidu,youdao}
+  $ translateMds [folder name] [API]{google,baidu,youdao} [options]
   default:
     API:youdao
 Example
-  $ translate-md-content md/ 
+  $ translateMds md/ 
   
+  [options]
+
+  -D debug 
 
 `);
 const APIs = ['google','baidu','youdao']
-let { api } = require('./config.js')
+let api = jsonText.api
+let jsonFile = './config.json'
 var dir = cli.input[0]
 if(!dir){
-  return console.log(chalk.green("--> V"+cli.pkg.version,cli.help))
-}else if(APIs.some(x => x == cli.input[1])){
-  api = cli.input[1]
+  return logger.info(chalk.green("--> V"+cli.pkg.version,cli.help))
+}else if(cli.flags['D']){
+  jsonText.logger.level = 'debug'
+  await writeJson(jsonFile, jsonText) 
+}else{
+  // rewrite config.json  
+  await writeJson(jsonFile, jsonText)
+  console.log('Rewrite')
 }
+APIs.forEach(x =>{
+  if( cli.input.join('\n').includes(x) ){
+    api = x
+  }
+})
 
 if(!dir.startsWith('/')){
 
   dir = '/' + dir
-  console.log(chalk.blue('Starting 翻译')+chalk.red(dir));
+  logger.info(chalk.blue('Starting 翻译')+chalk.red(dir));
 }
-// main func
 
-( async function(){
+// main func
 
 // get floder markdown files Array
 const getList = await Listmd(process.cwd()+dir)
@@ -79,7 +97,6 @@ getList.map(async (value) =>{
 
     writeDataToFile(head+'\n'+body, value) 
 
-    // console.log(chalk.red(bodyAll))
 
   })
 })
