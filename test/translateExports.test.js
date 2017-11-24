06b456cf185e7adb929d956099b073fd668f256d
chinanf-boy <865501259@qq.com>
2017-11-24 10:21:21 +0800

    add { absoluteFile, api, tranFrom, tranTo } options

--- test/translateExports.test.js
+++ test/translateExports.test.js
@@ -1,8 +1,8 @@
 const { test } = require('ava')
-
+const path = require('path')
 const translate = require('../bin/translateExports.js')
 
-let trueResult = `---\ntitle: Hugo Documentation\nlinktitle: Hugo\ndescription: Hugo is the world\'s fastest static website engine. It\'s written in Go (aka Golang) and developed by bep, spf13 and friends.\ndate: 2017-02-01\npublishdate: 2017-02-01\nlastmod: 2017-02-01\nmenu:\n  main:\n    parent: "section name"\n    weight: 01\nweight: 01\t#rem\ndraft: false\nslug:\naliases: []\ntoc: false\nlayout: documentation-home\n---\n雨果是一**世界上最快的静态网站引擎。**它是用Go(aka Golang)写成的[cep](https://github.com/bep)，[spf13](https://github.com/spf13)和[朋友](https://github.com/gohugoio/hugo/graphs/contributors)的。\n`
+let trueResult = `---\ntitle: Hugo Documentation\nlinktitle: Hugo\ndescription: Hugo is the world\'s fastest static website engine. It\'s written in Go (aka Golang) and developed by bep, spf13 and friends.\ndate: 2017-02-01\npublishdate: 2017-02-01\nlastmod: 2017-02-01\nmenu:\n  main:\n    parent: \"section name\"\n    weight: 01\nweight: 01\t#rem\ndraft: false\nslug:\naliases: []\ntoc: false\nlayout: documentation-home\n---\n雨果是**世界上最快的静态网站引擎。**它是用去（aka Golang）开发的[BEP](https://github.com/bep)，[spf13](https://github.com/spf13)和[朋友](https://github.com/gohugoio/hugo/graphs/contributors)。下面您将从我们的文档中找到一些最常见和最有用的页面。\n`
 
 test.failing('translate no absolute file fail', async t =>{
     let results = await translate(['./testWrite1.md'])
@@ -12,21 +12,37 @@ test.failing('translate input Object fail', async t =>{
     let results = await translate({})
     t.fail()
 })
-
-test('translate absolute file ', async t =>{
-    let results = await translate([__dirname+'/testWrite1.md'])
+test.serial('translate absolute file ', async t =>{
+    let results = await translate({aFile:__dirname+'/testWrite1.md', api:'baidu'})
     results = results.join('\n')
     t.is(JSON.stringify(results), JSON.stringify(trueResult))
 })
 
-test('translate absolute folder ', async t =>{
-    let results = await translate([__dirname+'/../md/','youdao'])
-    // console.log('-----------',results)
-    t.is(results.length,5)
+test.serial('translate absolute file from zh to en', async t =>{
+    let results = await translate({'aFile':__dirname+'/testWrite1.md',tF:'en',tT:'zh'})
+    t.is(results.length, 1)
 })
 
-test('translate absolute folder ', async t =>{
-    let results = await translate([__dirname+'/../md/','google'])
-    // console.log('-----------',results)
+test.serial('translate absolute folder auto', async t =>{
+    let results = await translate([path.resolve(__dirname,'../md/'),'baidu'], 'info')
     t.is(results.length,5)
 })
+// test async translate
+// while del serial , config.json is error
+// before data
+// {
+//     "from": "en",
+//     "to": "zh"
+// }
+// other write data
+// {
+//     "from": "en",
+//     "to": "zh",
+//      "t": "1"  
+// }
+// Data json show
+// {
+//     "from": "en",
+//     "to": "zh"
+// }"   // other write here is not emtpy
+// }
