c8acec33c74a5b28c05e060b07c1e226ed3aee2c
chinanf-boy <865501259@qq.com>
2018-03-25 16:45:14 +0800

    Fix lengthEqual trim /' /"

--- test/Fix/lengthEqual.test.js
+++ test/Fix/lengthEqual.test.js
@@ -3,38 +3,52 @@ const fs = require('fs')
 
 const { translateLengthEquals, mergeAndCut } = require('../../src/Fix/lengthEqual.js')
 
-test("mergeAndCut String asdf", t =>{
-    let a =["a","b","c","d","e","f","g"]
-    mergeAndCut(a, 2, 2)
-    t.deepEqual(a, ["a", "b", "cde", "f", "g"])
-})
+// test("mergeAndCut String asdf", t =>{
+//     let a =["a","b","c","d","e","f","g"]
+//     mergeAndCut(a, 2, 2)
+//     t.deepEqual(a, ["a", "b", "cde", "f", "g"])
+// })
 
-test("mergeAndCut String single", t =>{
-    let a =["asdf. ", "asdf. "]
-    mergeAndCut(a, 0, 2)
-    t.deepEqual(a, ["asdf. asdf. "])
-})
+// test("mergeAndCut String single", t =>{
+//     let a =["asdf. ", "asdf. "]
+//     mergeAndCut(a, 0, 2)
+//     t.deepEqual(a, ["asdf. asdf. "])
+// })
 
-test("translateLengthEquals String long", t =>{
-    let a =[`excited. This an awesome (rigorous and respectful) and curated (I read every suggestion and make judgement calls) list of cold showers on overhyped topics. This does `]
+// test("translateLengthEquals String long", t =>{
+//     let a =[`excited. This an awesome (rigorous and respectful) and curated (I read every suggestion and make judgement calls) list of cold showers on overhyped topics. This does `]
+//     let b = ["1","2","3","4"]
+//     translateLengthEquals(a, b)
+//     t.is(b.length, 2)
+// })
+
+test("translateLengthEquals String with \' or \"  ", t =>{
+    let a =[`"excited. This an awesome (rigorous and respectful) and curated (I read every suggestion and make judgement calls) list of cold showers on overhyped topics. This does "`]
     let b = ["1","2","3","4"]
     translateLengthEquals(a, b)
-    t.is(b.length, 2)
+    t.is(b.length, 4)
 })
 
-test("translateLengthEquals with '. '", t =>{
-    let b =["asdf. asdf. . "]
-    let c =["asdf. ", "asdf. ", ". "]
-    translateLengthEquals(b, c)
-    console.log(c)
-    t.deepEqual(c, ["asdf. asdf. . "])
+test("translateLengthEquals String with \' or \" has space", t =>{
+    let a =[` "excited. This an awesome (rigorous and respectful) and curated (I read every suggestion and make judgement calls) list of cold showers on overhyped topics. This does "`]
+    let b = ["1","2","3","4"]
+    translateLengthEquals(a, b)
+    t.is(b.length, 4)
 })
 
-test("translateLengthEquals with '. ' and '！' ", t =>{
-    let b =["asdf！. asdf. ！"]
-    let c =["asdf！", ". ", "asdf. ", "！"]
-    translateLengthEquals(b, c)
-    console.log(c)
-    t.deepEqual(c, ["asdf！. asdf. ！"])
-})
+// test("translateLengthEquals with '. '", t =>{
+//     let b =["asdf. asdf. . "]
+//     let c =["asdf. ", "asdf. ", ". "]
+//     translateLengthEquals(b, c)
+//     console.log(c)
+//     t.deepEqual(c, ["asdf. asdf. . "])
+// })
+
+// test("translateLengthEquals with '. ' and '！' ", t =>{
+//     let b =["asdf！. asdf. ！"]
+//     let c =["asdf！", ". ", "asdf. ", "！"]
+//     translateLengthEquals(b, c)
+//     console.log(c)
+//     t.deepEqual(c, ["asdf！. asdf. ！"])
+// })
 
