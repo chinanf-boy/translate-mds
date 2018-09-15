539662120481b95c90f8bb16c9b1fc614456bbf9
chinanf-boy <865501259@qq.com>
2018-09-15 21:44:28 +0800

    Up version: files-list

--- test/readmd.test.js
+++ test/readmd.test.js
@@ -2,42 +2,12 @@ const { test } = require('ava')
 
 const Listmd = require('../src/readmd.js')
 
-const opts = {deep:'all'}
-
 test("read zh md folder", async t =>{
-    const len = await Listmd(__dirname+"/../md/",opts).then(x => x)
-
+    const len = await Listmd(__dirname+"/../md/",{ deep: 'all' })
     t.is(len.length, 10)
 })
 
-test("read md no / folder", async t =>{
-    const len = await Listmd(__dirname+"/../md",opts).then(x => x)
-    t.is(len.length, 10)
-})
-
-test("read no absolute dir", async t =>{
-    const E = await Listmd("/../md",opts).then(x => x).catch(x =>x)
-    t.true(E instanceof Error)
-})
-
-test.serial.before("read md file", async t =>{
-    const len = await Listmd(__dirname+"/feature/testWrite.md", opts).then(x => x)
-
-
+test("read md file", async t =>{
+    const len = await Listmd(__dirname+"/feature/testWrite.md")
     t.is(len.length, 1)
-
-
-})
-
-// test.serial.before("read md no /", async t =>{
-
-//     const len2 = await Listmd(__dirname+"/../md").then(x => x)
-//     t.is(len2.length, 5)
-// })
-
-test("read md no Dir ",async t =>{
-
-    const error = await Listmd("/../md/",opts).catch(x => x)
-
-    t.true(error instanceof Error)
 })
