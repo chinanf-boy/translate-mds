7d63117e972322c60610eaea6847a9c8748bc143
chinanf-boy <865501259@qq.com>
2018-09-16 22:09:55 +0800

    Add test: --values , --translate

---
+++ test/translate-file/values.test.js
@@ -0,0 +1,15 @@
+import test from 'ava';
+process.chdir(__dirname+'/../../')
+
+const path = require('path')
+const execa = require('execa')
+
+test('use --values [path]', async t => {
+    let err = await t.throws(execa.shell('node cli.js ./test/translate-file/source.md --values -R'))
+    t.regex(err.message,/values save/)
+});
+
+test('use --translate [path]', async t => {
+    let err = await execa.shell('node cli.js ./test/translate-file/source.md --translate ./test/translate-file/translate.md -R')
+    t.regex(err.stderr,/be The translation/)
+});
