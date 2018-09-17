d01c0ff1d993d7aa1a7ae92d69a9cf2d6815c424
chinanf-boy <865501259@qq.com>
2018-09-17 09:18:33 +0800

    Fix CI: no /readme*.md

--- test/cli.test.js
+++ test/cli.test.js
@@ -3,24 +3,28 @@ const path = require('path')
 const execa = require('execa')
 process.chdir(path.resolve(__dirname+'/..'));
 
+let a = `./test/feature/testWrite3.en.md`
+let b = `./test/translate-file/translate-values.md`
+
+
 test('show help screen', async t => {
     t.regex(await execa.shell('node cli.js').then(x=>x.stdout), /translate/)
 });
 
 test('--glob', async t => {
-    t.regex(await execa.shell('node cli.js readme.md --glob *.js -D').then(x=>x.stderr), /glob/)
+    t.regex(await execa.shell(`node cli.js readme.md --glob *.js -D`).then(x=>x.stderr), /glob/)
 });
 
 test('--ignore', async t => {
-    t.regex(await execa.shell('node cli.js ./readme.md --ignore readme.md -D').then(x=>x.stderr), /ignore/)
+    t.regex(await execa.shell(`node cli.js ${b} --ignore ${b} -D`).then(x=>x.stderr), /ignore/)
 });
 
 test('已翻译', async t => {
-    t.regex(await execa.shell('node cli.js ./md -D').then(x=>x.stderr), /已翻译/)
+    t.regex(await execa.shell(`node cli.js ./md -D`).then(x=>x.stderr), /已翻译/)
 });
 
 test('国家后缀', async t => {
-    t.regex(await execa.shell('node cli.js ./readme.en.md -D').then(x=>x.stderr), /国家/)
+    t.regex(await execa.shell(`node cli.js ${a} -D`).then(x=>x.stderr), /国家/)
 });
 
 test('show version', async t => {
